92eecd7c5dc199cccb0a675c758fbecad544fb1e
jmmaniego <jmmaniego@hotmail.com>
2016-04-23 03:10:38 +0800

    setup layout for devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,18 @@
 class ApplicationController < ActionController::Base
+  layout :layout_by_resource
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
   before_action :authenticate_user!
+
+  protected
+
+  def layout_by_resource
+    if devise_controller?
+      "login"
+    else
+      "application"
+    end
+  end
 end
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,5 +1,4 @@
 class Users::SessionsController < Devise::SessionsController
-  layout "login"
 # before_filter :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
